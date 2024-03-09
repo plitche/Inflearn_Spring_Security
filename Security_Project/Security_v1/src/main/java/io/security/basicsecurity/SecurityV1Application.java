a73c8e5be56fc1aa2974150f311d18f06b9896f3
plitche <kysu728@naver.com>
2024-03-09 22:47:55 +0900

    인증 API - 프로젝트 구성 및 의존성 추가

---
+++ Security_Project/Security_v1/src/main/java/io/security/basicsecurity/SecurityV1Application.java
@@ -0,0 +1,15 @@
+package io.security.basicsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityV1Application.class, args);
+
+
+    }
+
+}
