417797cd93bb2d22c6caf3bd847a91ad462f9fc7
plitche <kysu728@naver.com>
2024-03-11 22:05:16 +0900

    인증 API - Logout, LogoutFilter

--- Security_Project/Security_v1/src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ Security_Project/Security_v1/src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -8,10 +8,13 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @Configuration // 설정 Class이기 때문에
@@ -47,6 +50,26 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     }
                 })
                 .permitAll()
+                ;
+
+        http
+                .logout() // 로그아웃 처리
+                .logoutUrl("/logout") // 로그아웃 처리 URL
+                .logoutSuccessUrl("/login") // 로그아웃 성공 후 이동페이지
+                .addLogoutHandler(new LogoutHandler() {
+                    @Override
+                    public void logout(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) {
+                        HttpSession session = httpServletRequest.getSession();
+                        session.invalidate(); // 세션 무효화
+                    }
+                })
+                .logoutSuccessHandler(new LogoutSuccessHandler() {
+                    @Override
+                    public void onLogoutSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
+                        httpServletResponse.sendRedirect("/login");
+                    }
+                })
+                .deleteCookies("remember-me") // 로그아웃 후 쿠키 삭제
                 ;
     }
 }
