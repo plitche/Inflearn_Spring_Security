3d9544d758d443812d73d4839fac7a2e50b56d7b
plitche <kysu728@naver.com>
2024-03-12 22:56:59 +0900

    인증 API - RememberMeAuthenticationFilter

--- Security_Project/Security_v1/src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ Security_Project/Security_v1/src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -40,53 +40,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated(); // 어떠한 요청에도 인증을 받는다 : 인가 정책
 
         http
-                .formLogin() // form Login 방식으로 인증 : 인증 정책
-                /*.loginPage("/loginPage") // 사용자 정의 로그인 페이지
-                .defaultSuccessUrl("/") // 로그인 성공 후 이동 페이지
-                .failureUrl("/loginPage") // 로그인 실패 후 이동 페이지
-                .usernameParameter("userId")
-                .passwordParameter("passwd")
-                .loginProcessingUrl("/login_proc") // 로그인 Form Action Url
-                .successHandler(new AuthenticationSuccessHandler() {
-                    @Override
-                    public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
-                        System.out.println("authentication : " + authentication.getName());
-                        httpServletResponse.sendRedirect("/");
-                    }
-                })
-                .failureHandler(new AuthenticationFailureHandler() {
-                    @Override
-                    public void onAuthenticationFailure(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException, ServletException {
-                        System.out.println("exception : " + e.getMessage());
-                        httpServletResponse.sendRedirect("loginPage");
-                    }
-                })
-                .permitAll()*/
-        ;
+                .formLogin(); // form Login 방식으로 인증 : 인증 정책
 
         http
-                .logout() // 로그아웃 처리
-                .logoutUrl("/logout") // 로그아웃 처리 URL
-                .logoutSuccessUrl("/login") // 로그아웃 성공 후 이동페이지
-                .addLogoutHandler(new LogoutHandler() {
-                    @Override
-                    public void logout(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) {
-                        HttpSession session = httpServletRequest.getSession();
-                        session.invalidate(); // 세션 무효화
-                    }
-                })
-                .logoutSuccessHandler(new LogoutSuccessHandler() {
-                    @Override
-                    public void onLogoutSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
-                        httpServletResponse.sendRedirect("/login");
-                    }
-                })
-                .deleteCookies("remember-me") // 로그아웃 후 쿠키 삭제
-        .and()
                 .rememberMe()
-                .rememberMeParameter("remember") // 기본값은 remember-me
-                .tokenValiditySeconds(3600)
-                .userDetailsService(userDetailsService)
-        ;
+                .userDetailsService(userDetailsService);
+
+
     }
 }
---
+++ Security_Project/Security_v1/src/main/java/io/security/basicsecurity/SecurityConfig_0311.java
@@ -0,0 +1,90 @@
+package io.security.basicsecurity;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+// @Configuration // 설정 Class이기 때문에
+@EnableWebSecurity
+public class SecurityConfig_0311 extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    UserDetailsService userDetailsService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated(); // 어떠한 요청에도 인증을 받는다 : 인가 정책
+
+        http
+                .formLogin() // form Login 방식으로 인증 : 인증 정책
+                /*.loginPage("/loginPage") // 사용자 정의 로그인 페이지
+                .defaultSuccessUrl("/") // 로그인 성공 후 이동 페이지
+                .failureUrl("/loginPage") // 로그인 실패 후 이동 페이지
+                .usernameParameter("userId")
+                .passwordParameter("passwd")
+                .loginProcessingUrl("/login_proc") // 로그인 Form Action Url
+                .successHandler(new AuthenticationSuccessHandler() {
+                    @Override
+                    public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
+                        System.out.println("authentication : " + authentication.getName());
+                        httpServletResponse.sendRedirect("/");
+                    }
+                })
+                .failureHandler(new AuthenticationFailureHandler() {
+                    @Override
+                    public void onAuthenticationFailure(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException, ServletException {
+                        System.out.println("exception : " + e.getMessage());
+                        httpServletResponse.sendRedirect("loginPage");
+                    }
+                })
+                .permitAll()*/
+        ;
+
+        http
+                .logout() // 로그아웃 처리
+                .logoutUrl("/logout") // 로그아웃 처리 URL
+                .logoutSuccessUrl("/login") // 로그아웃 성공 후 이동페이지
+                .addLogoutHandler(new LogoutHandler() {
+                    @Override
+                    public void logout(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) {
+                        HttpSession session = httpServletRequest.getSession();
+                        session.invalidate(); // 세션 무효화
+                    }
+                })
+                .logoutSuccessHandler(new LogoutSuccessHandler() {
+                    @Override
+                    public void onLogoutSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
+                        httpServletResponse.sendRedirect("/login");
+                    }
+                })
+                .deleteCookies("remember-me") // 로그아웃 후 쿠키 삭제
+        .and()
+                .rememberMe()
+                .rememberMeParameter("remember") // 기본값은 remember-me
+                .tokenValiditySeconds(3600)
+                .userDetailsService(userDetailsService)
+        ;
+    }
+}
