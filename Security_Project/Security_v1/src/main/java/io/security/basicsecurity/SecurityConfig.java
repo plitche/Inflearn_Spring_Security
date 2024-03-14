af4bcd902ac547e1dfa139ed1cbc6ce4bce0f159
plitche <kysu728@naver.com>
2024-03-14 21:53:22 +0900

    인가 API - 권한 설정 및 표현식

--- Security_Project/Security_v1/src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ Security_Project/Security_v1/src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -3,6 +3,7 @@ package io.security.basicsecurity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -22,10 +23,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
+    // in-memory 방식 사용자 생성
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        // (noop) : prefix(password encode 관련 내용)
+
+        auth.inMemoryAuthentication().withUser("user").password("(noop)1111").roles("USER");
+        auth.inMemoryAuthentication().withUser("sys").password("(noop)1111").roles("SYS", "USER");
+        auth.inMemoryAuthentication().withUser("admin").password("(noop)1111").roles("ADMIN", "SYS", "USER");
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
+                // 인가 정책
+                .antMatchers("/user").hasRole("USER") // /user 자원에 접근하면 USER 권한을 가진 사용자만 인가 처리
+                .antMatchers("/admin/pay").hasRole("ADMIN")
+                .antMatchers("/admin/**").access("hasRole('ADMIN') or hasRole('SYS')") // 구체적인 범위가 광범위한 범위보다 더 위에 위치해야 한다.
                 .anyRequest().authenticated(); // 어떠한 요청에도 인증을 받는다 : 인가 정책
 
         http
@@ -46,5 +61,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionManagement()
                 // .sessionFixation().none(); // 무방비 상태
                 .sessionFixation().changeSessionId(); // 기본 값
+
+
     }
 }
