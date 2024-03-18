706185de722223aaed630d165f34998d6a3b80b1
plitche <kysu728@naver.com>
2024-03-18 23:44:41 +0900

    SecurityContextHolder, SecurityContext

--- Security_Project/Security_v1/src/main/java/io/security/basicsecurity/SecurityController.java
+++ Security_Project/Security_v1/src/main/java/io/security/basicsecurity/SecurityController.java
@@ -1,13 +1,25 @@
 package io.security.basicsecurity;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
 
 @RestController
 public class SecurityController {
 
     @GetMapping("/")
-    public String index() {
+    public String index(HttpSession session) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        Object attribute = session.getAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY);
+        SecurityContext context = (SecurityContext) attribute;
+        Authentication authentication1 = context.getAuthentication();
+
         return "home";
     }
 
@@ -36,4 +48,17 @@ public class SecurityController {
         return "login";
     }
 
+    @GetMapping("/thread")
+    public String thread() {
+        new Thread(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+                    }
+                }
+        ).start();
+
+        return "thread";
+    }
 }
