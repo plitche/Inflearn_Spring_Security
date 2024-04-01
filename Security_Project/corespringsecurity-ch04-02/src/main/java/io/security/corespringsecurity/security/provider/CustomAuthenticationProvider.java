86683725aa2a39f7d2b5d20bb261b9f5a56aa8bf
plitche <kysu728@naver.com>
2024-04-01 23:15:35 +0900

    Form 인증 - CustomAuthenticationProvider

---
+++ Security_Project/corespringsecurity-ch04-02/src/main/java/io/security/corespringsecurity/security/provider/CustomAuthenticationProvider.java
@@ -0,0 +1,44 @@
+package io.security.corespringsecurity.security.provider;
+
+import io.security.corespringsecurity.security.service.AccountContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class CustomAuthenticationProvider implements AuthenticationProvider {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder; // 비밀번호 검증을 위해
+
+    // 검증을 위한 구현
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String username = authentication.getName(); // 로그인 할 때 입력한 아이디
+        String password = (String) authentication.getCredentials(); // 로그인 할 때 입력한 비밀번호
+
+        AccountContext accountContext = (AccountContext)userDetailsService.loadUserByUsername(username);
+        if (!passwordEncoder.matches(password, accountContext.getAccount().getPassword())) {
+            throw new BadCredentialsException("BadCredentialsException");
+        }
+
+        UsernamePasswordAuthenticationToken passwordAuthenticationToken
+                = new UsernamePasswordAuthenticationToken(accountContext.getAccount(), null, accountContext.getAuthorities());
+
+        return passwordAuthenticationToken;
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        // 토큰이 현재 파라미터로 전달 된 Class의 타입과 일치할때 CustomAuthenticationProvider 이 클래스가 인증을 처리하도록
+        return UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication);
+    }
+}
