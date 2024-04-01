ab97819292e051b5f1cecf33e80cdde2dbb8b502
plitche <kysu728@naver.com>
2024-04-01 22:55:09 +0900

    DB 연동 인증 처리(1) : CustomUserDetailService

--- Security_Project/corespringsecurity-ch04-02/src/main/java/io/security/corespringsecurity/domain/Account.java
+++ Security_Project/corespringsecurity-ch04-02/src/main/java/io/security/corespringsecurity/domain/Account.java
@@ -1,21 +1,39 @@
 package io.security.corespringsecurity.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.io.Serializable;
 
 @Entity // JPA에서 관리하는 Class가 되고, DB와 연결이 된다.
 @Data
-public class Account {
+@EqualsAndHashCode(of = "id")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Account implements Serializable {
 
     @Id
     @GeneratedValue
     private Long Id;
+
+    @Column
     private String username;
+
+    @Column
     private String password;
+
+    @Column
     private String email;
+
+    @Column
     private String age;
+
+    @Column
     private String role;
 }
